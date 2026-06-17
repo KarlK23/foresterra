b28e288bdaf0442679cacd6e25f7eade9ab3b748
Karl <kbrigou@gmail.com>
2026-06-17 14:37:23 +0200

    Fix Cloudinary back to raw type

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.post("/api/pdfs", requirePatron, function (req, res) {
     const id = genId("pdf");
     const {Readable} = require("stream");
     const stream = cloudinary.uploader.upload_stream(
-      { resource_type: "image", public_id: id, folder: "foresterra", format: "pdf" },
+      { resource_type: "raw", public_id: id, folder: "foresterra", use_filename: true, unique_filename: false },
       function(error, result) {
         if (error) return res.status(500).json({ error: "Cloudinary: " + error.message });
         const db = loadDb();
