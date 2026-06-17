1326715441d3f8c10b8e86bd93b0c5e435890d8a
Karl <kbrigou@gmail.com>
2026-06-17 15:18:37 +0200

    Fix crypto not defined

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const cloudinary = require("cloudinary").v2;
 const {Readable} = require("stream");
 cloudinary.config({cloud_name:process.env.CLOUDINARY_CLOUD_NAME,api_key:process.env.CLOUDINARY_API_KEY,api_secret:process.env.CLOUDINARY_API_SECRET});
